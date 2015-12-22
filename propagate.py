a2a1b2eae2e134e7137c85d195990190324b4023
Zander Moss <zander@mit.edu>
2015-12-22 16:10:06 -0500

    Ready to push

--- propagate.py
+++ propagate.py
@@ -27,8 +27,8 @@ eig_dcy=np.zeros(param.numneu)
 
 osc_test=True
 #osc_test=False
-#matter=False
-matter=True
+matter=False
+#matter=True
 
 eig_dcy[0]=3.1989727405321533e-07
 eig_dcy[1]=9.283607843296119e-07
@@ -40,28 +40,27 @@ eig_dcy[2]=6.567215979512332e-07
 
 
 
-nruns=100
+H=hamgen.gen(eig_dcy,param.GeV,matter)
 
-for x in range(0,nruns):
 
-	#Progress display
-	if (x+1)%((nruns)/10)==0:
-		print "Done: ",x+1,"/",nruns
-	
+#asolve = ApproxSolve.ApproxSolve(H,param)
+nsolve = NumSolve.NumSolve(H,param)
+desolve= DeSolve.DeSolve(H,param)
 
-	H=hamgen.gen(eig_dcy,param.GeV,matter)
-	
-	
-	#asolve = ApproxSolve.ApproxSolve(H,param)
-	nsolve = NumSolve.NumSolve(H,param)
-	desolve= DeSolve.DeSolve(H,param)
+dists=[]
+amps=[]
+deamps=[]
+
+for x in range(2,6):
+	res=10**x
+	print res
 	
 	#xdist=np.arange(0,param.EARTHRADIUS)
-	xdist=np.linspace(0,param.EARTHRADIUS,10000)
+	xdist=np.linspace(0,param.EARTHRADIUS,res)
 	#xdist=np.divide(xdist,10.0)	
 	dist=xdist
 	dist=dist*1000 #km to m
-
+	
 	
 	#we can just convert the distance to MKS, as it is the only
 	#parameter
@@ -79,58 +78,40 @@ for x in range(0,nruns):
 	for i in range(0,len(dist)):
 	#	a_amp[i] = asolve.P_ee(dist[i])
 		n_amp[i]= nsolve.scalar_prop(dist[i],0,0)
-	
+		
 	d_amp=desolve.prop(dist,0,0)
-	#print "RAW", len(d_amp)
-	#d_amp=d_amp[0:len(xdist)]
-
-	if osc_test:
-		fig, ax = plt.subplots()
-
-		ax.plot(xdist,n_amp,'r-',label='P(e->e): Diagonalized')
-		#ax.plot(xdist,a_amp,'b-',label='P(e->e): Approximate')
-		ax.plot(xdist,d_amp,'g--',label='P(e->e): Numerical')
-		
-		ax.set_xlabel("Distance (km)")
-		ax.set_ylabel("Oscillation Amplitude")
-		#ax.set_title("Evolution of 3 Flavors with a Random Hamiltonian")
-		#ax.set_title("Comparison of Numerical Evolution to AdG Approximation")
-		
-		plt.xlim([0,param.EARTHRADIUS])
-		plt.show()
-		break		
-
-	if x==0:
-		xrun=xdist
-		yrun=n_amp
-	else:
-		xrun=np.concatenate((xrun,xdist))
-		yrun=np.concatenate((yrun,n_amp))
-
-
-if (osc_test==False):	
-	nbinsx = 1000
-	nbinsy = 100
-	H, xedges, yedges = np.histogram2d(xrun,yrun,bins=(nbinsx,nbinsy))
+	print "RAW", len(d_amp)
+	d_amp=d_amp[0:len(xdist)]
 	
-	# H needs to be rotated and flipped
-	H = np.rot90(H)
-	H = np.flipud(H)
+	dists.append(xdist)
+	amps.append(n_amp)
+	deamps.append(d_amp)
 	
-	 
-	# Mask zeros
-	Hmasked = np.ma.masked_where(H==0,H) # Mask pixels with a value of zero
-	# Plot 2D histogram using pcolor
-	fig2 = plt.figure()
-	fig2.patch.set_facecolor('white')
-	plt.pcolormesh(xedges,yedges,Hmasked)
-	plt.xlabel('x')
-	plt.ylabel('y')
-	plt.xlabel("Distance (km)")
-	plt.ylabel("Oscillation Amplitude")
-	cbar = plt.colorbar()
-	cbar.ax.set_ylabel('Counts') 
-	plt.xlim([0,param.EARTHRADIUS])
-	plt.show()
+
+
+fig, ax = plt.subplots()
+
+colors=['r-','g-','b-','m-']
+colordash=['r--','g--','b--','m--']
+
+dlab=[]
+delab=[]
+for x in range(0,4):
+	dlab.append('Diagonalized 10^'+str(x+2))
+	delab.append('Numerical 10^'+str(x+2))
+	ax.plot(dists[x],amps[x],colors[x],label=dlab[x])
+	#ax.plot(xdist,a_amp,'b-',label='P(e->e): Approximate')
+	ax.plot(dists[x],deamps[x],colordash[x],label=delab[x])
 	
+ax.set_xlabel("Distance (km)")
+ax.set_ylabel("Oscillation Amplitude")
+#ax.set_title("Evolution of 3 Flavors with a Random Hamiltonian")
+#ax.set_title("Comparison of Numerical Evolution to AdG Approximation")
+
+# Now add the legend with some customizations.
+legend = ax.legend(loc='upper right', shadow=True)
+
+plt.xlim([0,param.EARTHRADIUS])
+plt.show()
+
 	
