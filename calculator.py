Made fixes so things match up with the schodinger case! Notably, the factor of one half in the von neumann equation in front of the gamma anticommutator did not belong there. Good to run!

calculator.py
@@ -24,7 +24,6 @@ plt.rc('font', size=16)
 param=pc.PhysicsConstants()
 
 #regen=False
-dcy_switch=True
 
 nu_mass = np.zeros(param.numneu, dtype=np.float64)
 nu_mass[0] = 0.0
@@ -40,12 +39,12 @@ pg.lamb[1,3] = math.pi/4.0
 pg.lamb[2,3] = math.pi/4.0
 
 dcy_channels = np.zeros((param.numneu,param.numneu),dtype=np.uint8)
-dcy_channels[0,1]=True
-dcy_channels[0,2]=True
+dcy_channels[0,1]=False
+dcy_channels[0,2]=False
 dcy_channels[0,3]=True
 dcy_channels[1,2]=False
-dcy_channels[1,3]=False
-dcy_channels[2,3]=False
+dcy_channels[1,3]=True
+dcy_channels[2,3]=True
 
 lifetime = 1.0e+2
 
@@ -53,9 +52,9 @@ tau = np.zeros((param.numneu,param.numneu),dtype=np.float64)
 tau[0,1]=lifetime
 tau[0,2]=lifetime
 tau[0,3]=lifetime
-#tau[1,2]=lifetime
-#tau[1,3]=lifetime
-#tau[2,3]=lifetime
+tau[1,2]=lifetime
+tau[1,3]=lifetime
+tau[2,3]=lifetime
 
 
 
@@ -129,12 +128,12 @@ num_vec=np.zeros(len(valvec))
 
 E = valvec*param.TeV
 print "E: ",E
-amps,sumamps= sp.AtmosphericNeutrinoOscillationProbability(3,3,E,param.PI,dcy_channels,tau,param,pg,nu_mass,phi_mass,regen,mtr_switch)
+amps= sp.AtmosphericNeutrinoOscillationProbability(3,3,E,param.PI,dcy_channels,tau,param,pg,nu_mass,phi_mass,regen,mtr_switch)
 
 num_vec = amps
 
 print "NV: ",num_vec
-print sumamps
+#print sumamps
 
 np.savez(calcvar+"_"+regenarg+"_von_neumann_spline_"+mattervar,_E=E,_potential=potential,_valvec=valvec,_num_vec=num_vec,_ntype=ntype)
 

run_analysis.py
@@ -35,16 +35,18 @@ else:
 """
 
 #Choice of matter and regeneration
+#regen=True
 regen=False
 mtr_switch=True
+#mtr_switch=False
 
 #Choice of initial and final flavors (mu in this case)
 flv_0 = 1
 flv_f = 1
 
-
 #Initialize PhysConst object
 param=pc.PhysicsConstants()
+param.numneu=4
 print "NUMNEU: ",param.numneu
 
 #Initialize masses
@@ -54,7 +56,8 @@ nu_mass[1] = math.sqrt(param.dm21sq)
 nu_mass[2] = math.sqrt(param.dm31sq)
 nu_mass[3] = 1.0
 phi_mass=0.0
-param.dm2[1,4] = nu_mass[3]**2 - nu_mass[0]**2
+param.dm2[1,4] = nu_mass[3]**2 - nu_mass[0]**2 #1ev^2
+
 
 #Initialize mixing angles (CHECK BY PRINTING pg.lamb that all are correct!!)
 pg=PMNSGen.PMNSGen(param)
@@ -65,26 +68,46 @@ pg.lamb[2,3] = math.pi/4.0
 print "LAMB: ",pg.lamb
 
 #Setting switches on which decay channels to use
+#Decay is from second index to first, like tau!
 dcy_channels = np.zeros((param.numneu,param.numneu),dtype=np.uint8)
-dcy_channels[0,1]=True
-dcy_channels[0,2]=True
+dcy_channels[0,1]=False
+dcy_channels[0,2]=False
 dcy_channels[0,3]=True
-dcy_channels[1,2]=True
+dcy_channels[1,2]=False
 dcy_channels[1,3]=True
 dcy_channels[2,3]=True
+print "DCYC: ",dcy_channels
 
 #This is a group lifetime for the time being
-lifetime = 1.0e+30
+lifetime = 1.0e+4
 
 #Setting decay channel lifetimes (mass basis)
 tau = np.zeros((param.numneu,param.numneu),dtype=np.float64)
-tau[0,1]=lifetime
-tau[0,2]=lifetime
+tau[0,1]=1e+60
+tau[0,2]=1e+60
 tau[0,3]=lifetime
-tau[1,2]=lifetime
+tau[1,2]=1e+60
 tau[1,3]=lifetime
 tau[2,3]=lifetime
 
+
+ntype=1
+
+if ntype==0:
+    pg.param.neutype='neutrino'
+    param.neutype='neutrino'
+elif ntype==1:
+    pg.param.neutype='antineutrino'
+    param.neutype='antineutrino'
+else:
+    print "BAD NEUTRINO TYPE"
+
+e_vec=param.TeV*np.logspace(-1,2,5)
+print "ENERGY: ",e_vec
+ret = sp.AtmosphericNeutrinoOscillationProbability(flv_0,flv_f,e_vec,math.pi,dcy_channels,tau,param,pg,nu_mass,phi_mass,regen,mtr_switch)
+
+print ret
+"""
 #Loading in energy and zenith edges for nusheep calculation
 edge_file = np.load("energy_zenith_edges.npz")
 e_vec = edge_file['e_edges']
@@ -141,6 +164,9 @@ detector_correction_path="/home/pinkpig/physics/neutrino_decay/nusheep_change/ne
 #Running the beast
 #Check years to run!
 V=Verosim.Verosim(param.numneu,0,0,data_path, flux_path, effective_area_path, detector_correction_path, nu_prob, nubar_prob)
+"""
+
+
 
 """
 #Minimizing Chi2